d8b32c8964a6d8717d40677b936809521eb241a9
hemanthaarya <137869271+hemanthaarya@users.noreply.github.com>
2025-05-16 17:24:29 +0530

    Add files via upload

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -20,7 +20,54 @@ function Home() {
             <div className='options'>
                 <h4>Home</h4>
                 <h4>About Us</h4>
-                <h4>Services</h4>
+                <div className='service'><h4>Services</h4>
+                    <div className='dropdown'>
+                        <div className='graphic'>
+                            <h2>Graphic Designing</h2>
+                            <ul>
+                                <li>Logo Design & Brand Guidelines</li>
+                                <li>Social Media templates & Creatives</li>
+                                <li>Posters, Flyers & Brochures</li>
+                                <li>Business Cards & Corporative Stationery</li>
+                                <li>UI/UX Design for Web & App Interfaces</li>
+                            </ul>
+                        </div>
+                        <div className='web'>
+                            <h2>Web & App Development</h2>
+                            <ul>
+                                <li>Web Design & Devolopment</li>
+                                <li>Landing pages & Portfolio Sites</li>
+                                <li>Android & iOS App Development</li>
+                                <li>Cross Platform App Development</li>
+                                <li>Backend Development & API Integration</li>
+                                <li>Performance Optimization & Maintenance</li>
+                            </ul>
+                        </div>
+                        <div className='marketing'>
+                            <h2>Marketing services</h2>
+                            <ul>
+                                <li>Social Media Strategy & Management</li>
+                                <li>Search Engine Optimization</li>
+                                <li>Google Ads & Paid Campaigns</li>
+                                <li>Email Marketing</li>
+                                <li>Bulk SMS Campaigns</li>
+                                <li>Content Creation & Scheduling</li>
+                                <li>Performance Monitoring & Analytics</li>   
+                            </ul>
+                        </div>
+                        <div className='data'>
+                            <h2>Data Analytics</h2>
+                            <ul>
+                                <li>Data Collection & Cleaning</li>
+                                <li>Dashboard Creation</li>
+                                <li>Customer & Sales Analytics</li>
+                                <li>Predictive & Trend Analysis</li>
+                                <li>Data Visualization & Custom Reports</li>
+                                <li>Business Intelligence Solutions</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
                 <h4>Blogs</h4>
                 <h4>Contact</h4>
             </div>
