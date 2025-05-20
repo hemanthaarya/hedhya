d90a4132b658b1e0d5d33b630e529d0769907ca2
hemanthaarya <137869271+hemanthaarya@users.noreply.github.com>
2025-05-20 16:50:17 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import Home from './Pages/HomePage';
+import Home from './Pages/HomePage.jsx';
+import {Helmet} from 'react-helmet';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>VIT HUB</title>
+      </Helmet>
+      <div>
+        <Home/>
+      </div>
+    </>
   );
 }
 
---
+++ src/menuData.js
@@ -0,0 +1,43 @@
+const menuData = [
+    {
+        id: 0,
+        title:'Services',
+        link: '#',
+        items: {
+           'Graphic Designing': [
+                'Graphic Logo Design & Brand Guidelines',
+                'Social Media Templates & Creatives',
+                'Posters, Flyers & Brochures',
+                'Business Cards & Corporate Stationery',
+                'UI/UX Design for Web & App Interfaces',
+                'Custom Illustrations & Infographics',
+            ],
+            'Web & App Development':[
+                 'Web Design & Development',
+                 'Landing Pages & Portfolio Sites',
+                 'Android & iOS App Development',
+                 'Cross-Platform App Development',
+                 'Backend Development & API Integration',
+                 'Performance Optimization & Maintenance',
+            ],
+            'Marketing services':[
+                 'Social Media Strategy & Management',
+                 'Search Engine Optimization',
+                 'Google Ads & Paid Campaigns',
+                 'Email Marketing',
+                 'Bulk SMS Campaigns',
+                 'Content Creation & Scheduling',
+                 'Performance Monitoring & Analytics',
+            ],
+            'Data Analytics':[
+                 'Data Collection & Cleaning',
+                 'Dashboard Creation',
+                 'Customer & Sales Analytics',
+                 'Predictive & Trend Analysis',
+                 'Data Visualization & Custom Reports',
+                 'Business Intelligence Solutions',
+            ]
+        }
+    },
+];
+export default menuData;
