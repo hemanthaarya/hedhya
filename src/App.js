cecc6e9127bd596b48ee18281b777cd85180d001
hemanthaarya <137869271+hemanthaarya@users.noreply.github.com>
2025-05-28 16:24:23 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './Pages/About.jsx';
 import Contact from './Pages/Contact.jsx';
 import Home from './Pages/HomePage.jsx';
 import Services from './Pages/Services.jsx';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
@@ -13,12 +14,15 @@ function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>VIT HUB</title>
       </Helmet>
-      <div>
-        <Home />
-        <About />
-        <Contact /> 
-        <Services />
-      </div>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
   );
 }
