e3879758d1f539d67913dff26c5f52dab61b7d72
hemanthaarya <137869271+hemanthaarya@users.noreply.github.com>
2025-07-25 16:30:18 +0530

    Add files via upload

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,18 +1,41 @@
 import Logo from '../Images/logo.jpg';
 import './Contact.css';
-import { BsTelephoneFill,BsFillChatTextFill,BsFillPinMapFill,BsWhatsapp,BsFacebook,BsTwitterX,BsLinkedin,BsInstagram,BsTelegram } from "react-icons/bs";
+import {
+  BsTelephoneFill, BsFillChatTextFill, BsFillPinMapFill,
+  BsWhatsapp, BsFacebook, BsTwitterX, BsLinkedin, BsInstagram, BsTelegram
+} from "react-icons/bs";
 import menuData from '../menuData';
 import Dropdown from './dropdown.jsx';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import IMG5 from '../Images/img5.jpg';
 import IMG6 from '../Images/img6.jpg';
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import emailjs from 'emailjs-com';
+
 
 function Contact() {
-    const [menuOpen, setMenuOpen] = useState(false);
-    
-    const toggleMenu = () => setMenuOpen(!menuOpen);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const form = useRef();
+  const SERVICE_ID = 'service_fci52ya';
+  const TEMPLATE_ID = 'template_lwfnnme';
+  const PUBLIC_KEY = 'Kb-L42B638FJ2WiLl';
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+      .then((result) => {
+          alert("Message sent successfully!");
+      }, (error) => {
+          alert("Something went wrong. Try again.");
+          console.error(error.text);
+      });
+
+    e.target.reset();
+  };
 
   return (
     <div className="contact">
@@ -32,7 +55,6 @@ function Contact() {
         </div>
 
         <nav className='navbar-2'>
-          {/* Hamburger Icon visible on mobile only */}
           <button 
             className='options1' 
             onClick={toggleMenu} 
@@ -44,7 +66,6 @@ function Contact() {
             <span className={`bar ${menuOpen ? 'open' : ''}`}></span>
           </button>
 
-          {/* Mobile Menu */}
           <div className={`mobile-menu ${menuOpen ? 'show' : ''}`}>
             <h4><Link to='/' className='Link' onClick={() => setMenuOpen(false)}>Home</Link></h4>
             <h4><Link to='/about' className='Link' onClick={() => setMenuOpen(false)}>About Us</Link></h4>
@@ -54,7 +75,6 @@ function Contact() {
             <h4><Link to='/contact' className='Link' onClick={() => setMenuOpen(false)}>Contact</Link></h4>
           </div>
 
-          {/* Desktop Menu */}
           <div className='options'>
             <h4><Link to='/' className='Link'>Home</Link></h4>
             <h4><Link to='/about' className='Link'>About Us</Link></h4>
@@ -68,94 +88,97 @@ function Contact() {
           </div>
         </nav>
       </header>
-        <div className='C-content'>
-            <div className='img-container'>
-                <img src={IMG5} alt='img' className='img3'/>
-                <div>
-                    <h1>CONTACT US</h1>
-                    <p>_____________________________________</p>
-                </div>
-            </div>
-            <div className='contact-us'>
-                <div className='form-container'>
-                    <div className='img-cont'>
-                        <img src={IMG6} alt='img'/>
-                    </div>
-                    <div>
-                        <div className='c_title'>
-                            <div></div>
-                            <h1>Get In Touch</h1>
-                        </div>
-                        <p>To help and answer any questions you might have. We look forward to hearing from you!</p>
-                        <div className='form'>
-                            <form className='form1'>
-                                <input type='text' placeholder='First-Name' className='input'/>
-                                <input type='text' placeholder='Last-Name' className='input'/>
-                            </form>
-                            <form className='form2'>
-                                <input type='email' placeholder='Email' className='input'/>
-                                <select type='select' placeholder='Services' className='input'>
-                                    <option value='select'>Graphic Designing</option>
-                                    <option value='select'>Web & App Development</option>
-                                    <option value='select'>Market Analysis</option>
-                                    <option value='select'>Data Analytics</option>
-                                </select>
-                                <textarea type='description' placeholder='Message' className='input'/>
-                                <button className='button'>Submit <FaArrowRight/></button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
+      <div className='C-content'>
+        <div className='img-container'>
+          <img src={IMG5} alt='img' className='img3'/>
+          <div>
+            <h1>CONTACT US</h1>
+            <p>_____________________________________</p>
+          </div>
         </div>
-        <footer className='footer'>
-            <div className='footer-up'>
-                <div className='contact'>
-                <div className='c_title'>
-                    <h1>Contact</h1>
-                </div>
-                <div>
-                    <h4><BsFillChatTextFill /><a href='mailto:vitdhub@gmail.com' style={{textDecoration:"none",color:"white"}}> vitdhub@gmail.com</a></h4>
-                    <h4><BsFillPinMapFill /> Vijayawada, AndhraPradesh, 521137</h4>
-                </div>
-                </div>
 
-                <div className='news_letter'>
-                <div className='c_title'>
-                    <h1>News Letter</h1>
-                </div>
-                <p>Subscribe to our newsletter for the latest updates and offers.</p>
-                <textarea placeholder='Subscribe to our channel' className='input' />
-                </div>
-
-                <div className='Pages'>
-                <div className='c_title'>
-                    <h1>Pages</h1>
-                </div>
-                <div className='pages'>
-                    <div><Link to='/' className='Link1'>Home</Link></div>
-                    <div><Link to='/about' className='Link1'>About</Link></div>
-                    <div><Link to='/about' className='Link1'>Team</Link></div>
-                    <div><Link to='/services' className='Link1'>Services</Link></div>
-                    <div><Link to='/CITR' className='Link1'>CITR</Link></div>
-                    <div><Link to='/contact' className='Link1'>Contact</Link></div>
-                </div>
-                </div>
+        <div className='contact-us'>
+          <div className='form-container'>
+            <div className='img-cont'>
+              <img src={IMG6} alt='img'/>
             </div>
+            <div>
+              <div className='c_title'>
+                <div></div>
+                <h1>Get In Touch</h1>
+              </div>
+              <p>To help and answer any questions you might have. We look forward to hearing from you!</p>
 
-            <div className='footer-down'>
-                <img src={Logo} alt='logo' className='logo1' />
-                <div className='icons'>
-                <a href='https://wa.me/message/OVK73JH6FGKZC1' target='_blank' rel="noopener noreferrer"><BsWhatsapp className='icon' /></a>
-                <a href='https://www.facebook.com/share/1C5RWvNczt/' target='_blank' rel="noopener noreferrer"><BsFacebook className='icon' /></a>
-                <a href='https://x.com/VITHUB_PVT_LTD?t=2AOtDnRqkNy5NcwNk6G-kg&s=08' target='_blank' rel="noopener noreferrer"><BsTwitterX className='icon' /></a>
-                <a href='https://www.linkedin.com/in/vijayawada-innovative-technology-development-hub-private-limited-89635536a' target='_blank' rel="noopener noreferrer"><BsLinkedin className='icon' /></a>
-                <a href='https://www.instagram.com/vithub_innovations?utm_source=qr&igsh=NTNza2RubnIyY2Vr' target='_blank' rel="noopener noreferrer"><BsInstagram className='icon' /></a>
-                <a href='t.me/vithubinnovations' target='_blank' rel="noopener noreferrer"><BsTelegram className='icon' /></a>
-                </div>
+              <form ref={form} onSubmit={sendEmail} className='form2'>
+                <input type='text' name='firstName' placeholder='First Name' className='input' required />
+                <input type='text' name='lastName' placeholder='Last Name' className='input' required />
+                <input type='email' name='email' placeholder='Email' className='input' required />
+                
+                <select name='service' className='input' required>
+                  <option value='Graphic Designing'>Graphic Designing</option>
+                  <option value='Web & App Development'>Web & App Development</option>
+                  <option value='Market Analysis'>Market Analysis</option>
+                  <option value='Data Analytics'>Data Analytics</option>
+                </select>
+
+                <textarea name='message' placeholder='Message' className='input' required></textarea>
+                <button className='button' type='submit'>Submit <FaArrowRight/></button>
+              </form>
             </div>
-        </footer>
+          </div>
+        </div>
+      </div>
+
+      <footer className='footer'>
+        <div className='footer-up'>
+          <div className='contact'>
+            <div className='c_title'>
+              <h1>Contact</h1>
+            </div>
+            <div>
+              <h4><BsFillChatTextFill /><a href='mailto:vitdhub@gmail.com' style={{textDecoration:"none",color:"white"}}> vitdhub@gmail.com</a></h4>
+              <h4><BsFillPinMapFill /> Vijayawada, AndhraPradesh, 521137</h4>
+            </div>
+          </div>
+
+          <div className='news_letter'>
+            <div className='c_title'>
+              <h1>News Letter</h1>
+            </div>
+            <p>Subscribe to our newsletter for the latest updates and offers.</p>
+            <textarea placeholder='Subscribe to our channel' className='input' />
+          </div>
+
+          <div className='Pages'>
+            <div className='c_title'>
+              <h1>Pages</h1>
+            </div>
+            <div className='pages'>
+              <div><Link to='/' className='Link1'>Home</Link></div>
+              <div><Link to='/about' className='Link1'>About</Link></div>
+              <div><Link to='/about' className='Link1'>Team</Link></div>
+              <div><Link to='/services' className='Link1'>Services</Link></div>
+              <div><Link to='/CITR' className='Link1'>CITR</Link></div>
+              <div><Link to='/contact' className='Link1'>Contact</Link></div>
+            </div>
+          </div>
+        </div>
+
+        <div className='footer-down'>
+          <img src={Logo} alt='logo' className='logo1' />
+          <div className='icons'>
+            <a href='https://wa.me/message/OVK73JH6FGKZC1' target='_blank' rel="noopener noreferrer"><BsWhatsapp className='icon' /></a>
+            <a href='https://www.facebook.com/share/1C5RWvNczt/' target='_blank' rel="noopener noreferrer"><BsFacebook className='icon' /></a>
+            <a href='https://x.com/VITHUB_PVT_LTD?t=2AOtDnRqkNy5NcwNk6G-kg&s=08' target='_blank' rel="noopener noreferrer"><BsTwitterX className='icon' /></a>
+            <a href='https://www.linkedin.com/in/vijayawada-innovative-technology-development-hub-private-limited-89635536a' target='_blank' rel="noopener noreferrer"><BsLinkedin className='icon' /></a>
+            <a href='https://www.instagram.com/vithub_innovations?utm_source=qr&igsh=NTNza2RubnIyY2Vr' target='_blank' rel="noopener noreferrer"><BsInstagram className='icon' /></a>
+            <a href='https://t.me/vithubinnovations' target='_blank' rel="noopener noreferrer"><BsTelegram className='icon' /></a>
+          </div>
+        </div>
+      </footer>
     </div>
-    )
+  );
 }
+
 export default Contact;
