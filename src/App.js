973101ea3db7b1717d7303562a182b8e878bb738
hemanthaarya <137869271+hemanthaarya@users.noreply.github.com>
2025-07-29 15:04:45 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <>
       <Helmet>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>VIT HUB</title>
+        <title>VIJAYAWADA INNOVATIVE TECHNOLOGY DEVELOPMENT HUB PRIVATE LIMITED</title>
       </Helmet>
 
       <Router>
